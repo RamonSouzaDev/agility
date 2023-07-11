91f22acfb902d194770b4a3b003945afa0dc90ea
Ramon Mendes <dwmom@hotmail.com>
2023-07-11 18:01:54 -0300

    feat: adicionando cadastro de usuários

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        return response()->json(['message' => 'User registered successfully', 'user' => $user], 201);
+    }
+}
+
