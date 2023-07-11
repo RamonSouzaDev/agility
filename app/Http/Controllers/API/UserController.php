7db41cb5ff818ad1e384b553a9be1c3b8af0faf0
Ramon Mendes <dwmom@hotmail.com>
2023-07-11 18:04:32 -0300

    feat: adicionando rota de login de usuários

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -23,6 +24,20 @@ class UserController extends Controller
         ]);
 
         return response()->json(['message' => 'User registered successfully', 'user' => $user], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('AuthToken')->plainTextToken;
+
+            return response()->json(['message' => 'Login successful', 'user' => $user, 'token' => $token], 200);
+        } else {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
     }
 }
 
